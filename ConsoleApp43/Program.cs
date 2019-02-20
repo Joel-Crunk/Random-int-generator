1c002597cfb5c7cbebd1d8bf5e68e8b44ff6a433
Joel-Crunk <joel.crunk@outlook.com>
2019-02-20 14:34:03 -0500

    This commit will generate a random number between 0 and 2.

--- ConsoleApp43/Program.cs
+++ ConsoleApp43/Program.cs
@@ -10,6 +10,24 @@ namespace ConsoleApp43
     {
         static void Main(string[] args)
         {
+            Random random = new Random();
+           // random.Next(0, 2);
+            int myRandom = random.Next(0, 2);
+
+            if (myRandom == 0)
+            {
+                Console.WriteLine("0");
+            }
+            else if  (myRandom == 1)
+            {
+                Console.WriteLine("1");
+            }
+            else 
+            {
+                Console.WriteLine("2");
+            }
+            Console.ReadLine();
+
         }
     }
 }
